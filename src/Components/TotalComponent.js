5bd51097f5f73de31f0b93078eae704a53302d99
Jilmer-Mayta-Condori <jilmer.mayta@tecsup.edu.pe>
2021-05-30 12:51:44 -0500

    6. Agregando los Objetos en un Array

--- src/Components/TotalComponent.js
+++ src/Components/TotalComponent.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Total = (props) => {
+const Total = ({parts}) => {
     return(
         <div>
-            <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+            <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
         </div>
     )
 }
