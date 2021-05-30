a8fe41587ab120e7345891631a0508bac8354362
Jilmer-Mayta-Condori <jilmer.mayta@tecsup.edu.pe>
2021-05-30 12:13:00 -0500

    3. Refactorizando el codigo con Componentes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Header from './Components/HeaderComponent'
+import Content from './Components/ContentComponent'
+import Total from './Components/TotalComponent'
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -12,7 +15,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
+      {/* <h1>{course}</h1>
       <p>
         {part1} {exercises1}
       </p>
@@ -22,7 +25,12 @@ const App = () => {
       <p>
         {part3} {exercises3}
       </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
 }
